a9fbd98ad055056c2762f4a9b7b11ae7a73898f4
Vitor Monteiro <mluizvitor@gmail.com>
2022-03-05 17:37:46 -0300

    Fix: Geração de IDs ausente no commit anterior

---
+++ src/components/utils/genId.ts
@@ -0,0 +1,12 @@
+export function genId() {
+  const charBase =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  let newId = "";
+
+  for (let i = 0; i < 10; i++) {
+    newId += charBase.charAt(Math.floor(Math.random() * charBase.length));
+  }
+
+  return newId;
+}
